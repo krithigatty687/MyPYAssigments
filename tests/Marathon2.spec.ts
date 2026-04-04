6384d95e55778bb12ed6bdbf223b9c8864d40467
krithi <gattikrithi@gmail.com>
2026-04-04 15:00:37 +0530

    Decathlon lines added

--- tests/Marathon2.spec.ts
+++ tests/Marathon2.spec.ts
@@ -2,5 +2,6 @@ import {test,expect} from '@playwright/test'
 test("Search product, apply filters, add to cart in Decathlon",async({page})=>
 {
 await page.goto("https://www.decathlon.in/")
-
+const title=await page.title()
+console.log(title)
 })
